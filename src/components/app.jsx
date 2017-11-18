58e8ab421bd18a8e7b3f0888263060fd471fe12f
sa.nitawaki <fsrgrow@gmail.com>
2017-11-18 20:24:28 +0900

    Lesson18. Use stete.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'Bob',
+    }
+  }
+
   render() {
     return (
       <div>
-        <Greeting name="John" />
-        <Greeting name="Bob" />
+        <Greeting name= {this.state.name} />
+        <Greeting name="Bob2" />
       </div>
     );
   }
