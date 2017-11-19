af28af7d6d156fdfed09153d7b364b2d3a61a326
sa.nitawaki <fsrgrow@gmail.com>
2017-11-20 08:30:11 +0900

    Lesson23. Add GeocodeResult component.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
 
-import Greeting from './greeting';
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'John',
     };
   }
 
-  handleNameChange(name) {
-    this.setState({ name });
-    // this.setState({ name: e.target.value});
-  }
-
-  handleBobClicked() {
-    this.setState({ name: 'Bob' });
+  handlePlaceChange(place) {
+    console.log(place);
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceChange(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
         />
-        <button onClick={() => this.handleBobClicked()}> I am Bob</button>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
