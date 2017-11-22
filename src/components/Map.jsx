a1ab791dcc83e9a9ebe73b756d94a609f8e96816
sa.nitawaki <fsrgrow@gmail.com>
2017-11-22 16:57:59 +0900

    GoogleMapの初期表示を設定 検索を実行すると検索結果のMapが表示されるように設定

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 const InnerMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={12}
-    defaultCenter={{ lat: 35.6585805, lng: 139.7454329 }}
-  >
-  </GoogleMap>
+    defaultCenter={props.position}
+    center={props.position}
+  />
 ));
 
-const Map = props => (
+const Map = ({ lat, lng }) => (
   <InnerMap
     containerElement={(<div />)}
     mapElement={(<div className="map" />)}
+    position={{ lat, lng }}
   />
 );
+
+Map.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+};
+
+Map.defaultProps = {
+  // 東京タワーの緯度/経度
+  lat: 35.6585805,
+  lng: 139.7454329,
+};
 
 export default Map;
